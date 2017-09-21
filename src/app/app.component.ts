928a30d6c07a186faf012fc622e023ba17e2a3d6
Matias Gea <matigea@gmail.com>
2017-09-20 23:19:36 -0300

    Quote component now accepts data through an input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public quote: {quote: string, author: string, cat: string} = {
+    quote: 'Respect is one of the greatest expressions of love.',
+    author: 'Miguel Angel Ruiz',
+    cat: 'respect'
+  };
   title = 'app';
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,14 +1,10 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent {
-  public quote: {quote: string, author: string, cat: string} = {
-      quote: 'Respect is one of the greatest expressions of love.',
-      author: 'Miguel Angel Ruiz',
-      cat: 'respect'
-  };
- }
+  // Define a component input binding
+  @Input() quote: {quote: string, author: string, cat: string};
+}
