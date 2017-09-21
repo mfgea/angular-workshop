a38d9126dcd5e5ac2bd41996ba57130fdbb37bcb
Matias Gea <matigea@gmail.com>
2017-09-21 00:11:38 -0300

    Added ngIf directive to the button

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -7,6 +7,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 export class QuoteComponent {
   // Define a component input binding
   @Input() quote: {quote: string, author: string, cat: string};
+  @Input() showButton: boolean;
 
   // Define a component output binding, able to emit an event
   @Output() onQuoteChange = new EventEmitter<any>();
