766e706cafb5efacf84b9522d6d15881441bb322
Matias Gea <matigea@gmail.com>
2017-10-02 15:52:30 -0300

    Add routing to our app and test it by setting the desired route in the address bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuotesService } from './quotes.service';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+ ];
 
 @NgModule({
   declarations: [
@@ -17,7 +23,10 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [QuotesService],
   bootstrap: [AppComponent]
